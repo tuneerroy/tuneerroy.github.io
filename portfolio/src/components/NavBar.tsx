55c9ee267fe057999082a5cd4e5249f5908d5a6a
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2022-10-25 17:58:02 -0400

    style update

--- portfolio/src/components/NavBar.tsx
+++ portfolio/src/components/NavBar.tsx
@@ -1,17 +1,17 @@
 import { Navbar, Nav } from 'react-bootstrap';
 
 const NavBar = () => {
-  return (
-    <Navbar>
-      <Navbar.Brand href="#home">Tuneer Roy</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse className="justify-content-end">
-        <Nav>
-          <Nav.Link href="#projects">Projects</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  )
+    return (
+        <Navbar>
+            <Navbar.Brand href="#home">Tuneer Roy</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse className="justify-content-end">
+                <Nav>
+                    <Nav.Link href="#projects">Projects</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
 }
 
 export default NavBar;

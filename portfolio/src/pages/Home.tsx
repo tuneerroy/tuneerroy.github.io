af61ab737888fe74e243c0ed77ff54529b4af88e
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2022-12-05 20:17:40 -0500

    update profile

--- portfolio/src/pages/Home.tsx
+++ portfolio/src/pages/Home.tsx
@@ -37,7 +37,7 @@ const Home = () => {
                 </Row>
                 <Row>
                     <p>
-                        Welcome to my portfolio! I'm a sophomore at the University of Pennsylvania studying <PhraseLink href="https://www.cis.upenn.edu/">Computer Science</PhraseLink> with an expected graduation in May 2024.
+                        Welcome to my portfolio! I'm a student at the University of Pennsylvania studying <PhraseLink href="https://www.cis.upenn.edu/">Computer Science</PhraseLink>.
                     </p>
                     <p>
                         Currently, I work as a backend developer as part of <PhraseLink href="https://pennlabs.org/">Penn Labs</PhraseLink> where I write RESTful API for the <PhraseLink href="https://pennlabs.org/products/penn-mobile">Penn Mobile</PhraseLink> application, Penn's official student app. I am also a Teaching Assistant for the <PhraseLink href="https://catalog.upenn.edu/courses/cis/#:~:text=CIS%202620">Automata, Computability, and Complexity</PhraseLink> course at UPenn. Below, you can find most recent projects.

1f4c717a6befd336d65abb0c781a5f4f9980ac69
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2023-06-20 19:46:30 -0700

    feat: zoom redirection link

--- portfolio/src/index.tsx
+++ portfolio/src/index.tsx
@@ -1,16 +1,30 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-)
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const ZoomRedirect = () => {
+    window.location.href = 'https://upenn.zoom.us/j/8111458210'
+    return null
+}
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/zoom",
+        element: <ZoomRedirect />,
+    }
+])
+
+createRoot(document.getElementById("root") as Element).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
